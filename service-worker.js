019487a21fe1162e583153fcec65886175395d66
jotaifpb <87494107+jotaifpb@users.noreply.github.com>
2026-03-08 13:31:19 -0300

    Register service worker in the browser

--- service-worker.js
+++ service-worker.js
@@ -34,3 +34,8 @@ return fetch(event.request);
 );
 
 });
+
+if ('serviceWorker' in navigator) {
+navigator.serviceWorker.register('service-worker.js')
+.then(() => console.log("Service Worker registrado"));
+}
